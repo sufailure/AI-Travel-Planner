618ada1c5362533746016e1f39d9824c87cef097
zhusaisai <211250229@smail.nju.edu.cn>
2025-10-20 10:35:23 +0800

    chore: adjust config for Next.js runtime

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 const config = {
     plugins: {
-        tailwindcss,
-        autoprefixer,
+        tailwindcss: {},
+        autoprefixer: {},
     },
 };
 
